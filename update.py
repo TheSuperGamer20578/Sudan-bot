eb049bace4fcd45ba1c27150c680517a06930718
TheSuperGamer20578 <other@thesupergamer20578.ml>
2020-10-05 15:21:57 +1100

    Fix style errors in update

--- update.py
+++ update.py
@@ -1,17 +1,25 @@
-import discord
+"""
+Automatically updates the bot when there are changes to master
+"""
 from discord.ext import commands
 from subprocess import call
-from core import trusted
+from .core import trusted
 import time
 from asyncio import sleep
 
 
 class update(commands.Cog):
+    """
+    Main class
+    """
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        """
+        Checks for updates
+        """
         if message.channel.id == 761747494693765151 and message.embeds:
             if " new commit" in message.embeds[0].title and message.embeds[0].title.startswith("[Sudan-bot:master] "):
                 await sleep(15)
@@ -21,6 +29,9 @@ class update(commands.Cog):
     @commands.command()
     @commands.check(trusted)
     async def forceupdate(self, ctx):
+        """
+        Forces the bot to download an update
+        """
         t = time.time()
         msg = await ctx.send("updating...")
         call(["git", "pull"])
@@ -29,10 +40,15 @@ class update(commands.Cog):
     @commands.command()
     @commands.check(trusted)
     async def stopbot(self, ctx):
+        """
+        Stops the bot
+        """
         await ctx.send("stopping...")
         await self.bot.close()
 
 
 def setup(bot):
-    gbot = bot
+    """
+    Initialize cog
+    """
     bot.add_cog(update(bot))
