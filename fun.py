f15b1775e0af2dc4a9d2145d0947d794b836dc62
TheSuperGamer20578 <other@thesupergamer20578.ml>
2020-10-05 11:53:12 +1100

    Added reactions to chain

--- fun.py
+++ fun.py
@@ -60,6 +60,7 @@ class fun(commands.Cog):
         async def cbreak():
             if msg.content[1:] == f"chain {d['chain'][str(msg.channel.id)]}":
                 return
+            await msg.add_reaction("❌")
             await msg.channel.send(f"{msg.author.mention} broke the chain! start a new chain with `.chain <thing>`")
             s = settings.document(str(msg.guild.id)).get().to_dict()
             if "breakrole" in s:
@@ -82,6 +83,10 @@ class fun(commands.Cog):
                 await cbreak()
             else:
                 last_chain[str(msg.channel.id)] = msg.author.id
+                if msg.content == "100":
+                    await msg.add_reaction("💯")
+                else:
+                    await msg.add_reaction("✅")
 
 
 def setup(bot):
