fa00f423e366e50bb85d4457fe3e808a1ffa1ceb
TheSuperGamer20578 <other@thesupergamer20578.ml>
2021-01-02 18:53:12 +1100

    made log.py work with .env

--- bot/log.py
+++ bot/log.py
@@ -8,7 +8,7 @@ from firebase_admin import firestore, credentials, initialize_app
 from core import trusted, admin, GREEN
 
 try:
-    cred = credentials.Certificate("Config/firebase.json")
+    cred = credentials.Certificate("firebase.json")
     initialize_app(cred)
 except ValueError:
     pass
