c9601e4e66948f492113dcf6b8ea88db8195be19
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-01-05 16:56:40 +1100

    made fun.py work with new db and fixed several issues with it SDB-66 SDB-71 SDB-72 SDB-95 SDB-100 SDB-105

--- bot/fun.py
+++ bot/fun.py
@@ -2,27 +2,17 @@
 Contains fun stuff
 """
 import re
+import asyncio
 
 import discord
 from discord.ext import commands
-from firebase_admin import firestore, credentials, initialize_app
-
-last_chain = {}
-
-try:
-    cred = credentials.Certificate("firebase.json")
-    initialize_app(cred)
-except ValueError:
-    pass
-db = firestore.client()
-fs_data = db.collection("fun")
-settings = db.collection("settings")
 
 
 class fun(commands.Cog):
     """
     The main class for this file
     """
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -32,19 +22,23 @@ class fun(commands.Cog):
         When someone bothers you with their questions use this command to link them to google after they have a little laugh
         """
         await ctx.message.delete()
-        embed = discord.Embed(title=query, url=f"https://lmgtfy.com/?q={query.replace(' ', '%20')}&pp=1&iie=1")
+        embed = discord.Embed(title=query,
+                              url=f"https://lmgtfy.com/?q={query.replace(' ', '%20')}&pp=1&iie=1")
         embed.set_author(name=ctx.author.nick, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.check(lambda ctx: "sudont" not in [role.name for role in ctx.author.roles])
+    @commands.check(
+        lambda ctx: "sudont" not in [role.name for role in ctx.author.roles])
     async def sudo(self, ctx, user: discord.Member, *, message):
         """
         Mimics another user
         """
         await ctx.message.delete()
-        webhook = await ctx.channel.create_webhook(name=user.nick if user.nick is not None else user.name)
-        await webhook.send(message.replace("@", "@\N{zero width space}"), avatar_url=user.avatar_url)
+        webhook = await ctx.channel.create_webhook(
+            name=user.nick if user.nick is not None else user.name)
+        await webhook.send(message.replace("@", "@\N{zero width space}"),
+                           avatar_url=user.avatar_url)
         await webhook.delete()
 
     @commands.command()
@@ -53,82 +47,91 @@ class fun(commands.Cog):
         Starts a chain
         """
         if "@" in thing:
-            return await ctx.send("Nice try but you cant fool me into mentioning")
-        data = fs_data.document(str(ctx.guild.id)).get().to_dict()
-        if str(ctx.channel.id) in data["chain"]:
-            return
-        if data is None:
-            data = {"chain": {}}
-        data["chain"][str(ctx.channel.id)] = thing
-        last_chain[str(ctx.channel.id)] = None
-        fs_data.document(str(ctx.guild.id)).set(data)
+            return await ctx.send(
+                "Nice try but you cant fool me into mentioning")
+        await self.bot.db.execute("UPDATE channels SET last_chain = NULL WHERE id = $1", ctx.channel.id)
+        await self.bot.db.execute(
+            "UPDATE channels SET chain = $2 WHERE id = $1", ctx.channel.id,
+            thing)
         if thing.startswith("$counting:"):
             await ctx.send(f"Counting started at: {thing.split(':')[1]}")
         else:
             await ctx.send(f"New chain: {thing}")
 
-    @commands.Cog.listener()
-    async def on_message(self, msg):
+    async def chain_check(self, msg):
         """
         Checks to see if chain is broken
         """
+        chain = await self.bot.db.fetchval("SELECT chain FROM channels WHERE id = $1", msg.channel.id)
+
         async def cbreak():
             """
             If the chain is broken say so and update DB
             """
-            if msg.content[1:] == f"chain {data['chain'][str(msg.channel.id)]}":
+            if msg.content.endswith(f"chain {chain}"):
                 return
             await msg.add_reaction("❌")
-            await msg.channel.send(f"{msg.author.mention} broke the chain! start a new chain with `.chain <thing>`")
-            setting = settings.document(str(msg.guild.id)).get().to_dict()
-            if "breakrole" in setting:
-                await msg.author.add_roles(msg.guild.get_role(int(setting["breakrole"])))
-            del data["chain"][str(msg.channel.id)]
-            fs_data.document(str(msg.guild.id)).set(data)
-        data = fs_data.document(str(msg.guild.id)).get().to_dict()
-        if data is None or msg.author.bot:
+            await msg.channel.send(
+                f"{msg.author.mention} broke the chain! start a new chain with `.chain <thing>`. Chain length: {await self.bot.db.fetchval('SELECT chain_length FROM channels WHERE id = $1', msg.channel.id)}")
+            break_role = await self.bot.db.fetchval("SELECT chain_break_role FROM guilds WHERE id = $1", msg.guild.id)
+            if break_role is not None:
+                await msg.author.add_roles(msg.guild.get_role(break_role))
+            await self.bot.db.execute(
+                "UPDATE channels SET chain = NULL, chain_length = 0 WHERE id = $1",
+                msg.channel.id)
+
+        if chain is None or msg.author.bot:
             return
-        if str(msg.channel.id) in data["chain"]:
-            if data["chain"][str(msg.channel.id)].startswith("$counting:"):
-                num = int(data["chain"][str(msg.channel.id)].split(":")[1])
-                if msg.content != str(num+1):
-                    await cbreak()
-                else:
-                    num += 1
-                    data["chain"][str(msg.channel.id)] = f"$counting:{num}"
-                    fs_data.document(str(msg.guild.id)).set(data)
-            elif msg.content != data["chain"][str(msg.channel.id)] or msg.author.id == last_chain[str(msg.channel.id)]:
+        if chain.startswith("$counting:"):
+            num = int(chain.split(":")[1])
+            if msg.content != str(num + 1):
                 await cbreak()
             else:
-                last_chain[str(msg.channel.id)] = msg.author.id
-                if msg.content == "100":
-                    await msg.add_reaction("💯")
-                else:
-                    await msg.add_reaction("✅")
+                num += 1
+                await self.bot.db.execute("UPDATE channels SET chain = $2 WHERE id = $1", msg.channel.id, f"$counting:{num}")
+        elif msg.content != chain or msg.author.id == await self.bot.db.fetchval("SELECT last_chain FROM channels WHERE id = $1", msg.channel.id):
+            await cbreak()
+        else:
+            await self.bot.db.execute("UPDATE channels SET last_chain = $2, chain_length = chain_length + 1 WHERE id = $1", msg.channel.id, msg.author.id)
+            if msg.content == "100":
+                await msg.add_reaction("💯")
+            else:
+                await msg.add_reaction("✅")
+
+    async def dad_mode(self, message):
+        """
+        dad mode
+        """
+        if not await self.bot.db.fetchval("SELECT dad_mode FROM users WHERE id = $1", message.author.id):
+            return
+        phrases = {
+            "i am {}": "Hi {}! I am Dad",
+            "i'm {}": "Hi {}! I'm Dad",
+            "im {}": "Hi {}! Im Dad",
+            "わたしは{}です": "こんにちわ{}さん！わたしはおとうさんです",
+            "私は{}です": "こんにちわ{}さん！私はお父さんです"
+        }
+        for said, reply in zip(phrases.keys(), phrases.values()):
+            match = re.search(said.format(r"(.*)"), message.content.lower())
+            if match:
+                await message.channel.send(reply.format(match.group(1)))
 
     @commands.Cog.listener()
     async def on_message(self, message):
         """
-        dad mode
+        runs stuff that has to run on_message
         """
-        # pylint: disable=function-redefined
-        if str(message.author.id) not in fs_data.document("dad").get().to_dict():
-            return
-        match = re.search(r"i am (.*)", message.content.lower())
-        if match:
-            await message.channel.send(f"Hi {match.group(1)}! Im Dad")
+        await asyncio.sleep(.1)
+        await self.dad_mode(message)
+        await asyncio.sleep(.1)
+        await self.chain_check(message)
 
     @commands.command()
     async def dadmode(self, ctx):
         """
         Toggles Dad mode.
         """
-        data = fs_data.document("dad").get().to_dict()
-        if str(ctx.author.id) in data:
-            del data[str(ctx.author,id)]
-        else:
-            data[str(ctx.author.id)] = True
-        fs_data.document("dad").set(data)
+        await self.bot.db.execute("UPDATE users SET dad_mode = NOT dad_mode WHERE id = $1", ctx.author.id)
         await ctx.send("dad toggled!")
 
 
