21c37da3b0571bb0b81f22de5b86d5bf36550397
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-04-02 15:08:01 +1100

    Fixed .env not loading for setup.py thanks pylint

--- bot/setup.py
+++ bot/setup.py
@@ -3,6 +3,7 @@ from os import getenv
 from requests import post
 from dotenv import load_dotenv
 
+load_dotenv()
 DISCORD = f"https://discord.com/api/v8/applications/{getenv('APPLICATION_ID')}"
 HEADERS = {"Authorization": f"Bot {getenv('BOT_TOKEN')}"}
 
