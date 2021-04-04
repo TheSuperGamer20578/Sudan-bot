6a10a8f53a7c7e433275434d8cdfa0ccf4eb57a6
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-04-05 09:38:14 +1000

    fixed stuff on the embed

--- bot/settings.py
+++ bot/settings.py
@@ -26,14 +26,14 @@ class settings(commands.Cog):
         embed = discord.Embed(title="Settings")
         if Checks.admin(ctx):
             settings = await self.bot.db.fetchrow("SELECT * FROM guilds WHERE id = $1", ctx.guild.id)
-            embed.add_field(name="Server settings", value=f"""
-                Admin roles: {', '.join(f'<@&{settings["admin_roles"]}>')}
-                Moderator roles: {', '.join(f'<@&{settings["mod_roles"]}>')}
+            embed.add_field(name="Server settings", inline=False, value=f"""
+                Admin roles: {', '.join([f'<@&{role}>' for role in settings["admin_roles"]])}
+                Moderator roles: {', '.join([f'<@&{role}>' for role in settings["mod_roles"]])}
                 Ticket support role: {f'<@&{settings["support_role"]}>'}
                 Chain break role: {f'<@&{settings["chain_break_role"]}>'}
             """)
         settings = await self.bot.db.fetchrow("SELECT * FROM users WHERE id = $1", ctx.author.id)
-        embed.add_field(name="User settings", value=f"""
+        embed.add_field(name="User settings", inline=False, value=f"""
             Dad mode: {'🟢' if settings['dad_mode'] else '🔴'}
         """)
         embed.set_author(name=ctx.author.nick if ctx.author.nick else ctx.author.name, icon_url=ctx.author.avatar_url)
