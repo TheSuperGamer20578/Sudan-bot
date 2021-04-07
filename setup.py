8f47929eb74e7fef702e1a84602695cbedf0dfc5
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-04-07 19:40:50 +1000

    defined /set

--- setup.py
+++ setup.py
@@ -9,25 +9,25 @@ DISCORD = f"https://discord.com/api/v8/applications/{getenv('APPLICATION_ID')}"
 HEADERS = {"Authorization": f"Bot {getenv('BOT_TOKEN')}"}
 DELAY = 3
 
-post(DISCORD+"/commands", headers=HEADERS, json={
+post(DISCORD + "/commands", headers=HEADERS, json={
     "name": "ping",
     "description": "Ping! Pong!"
 })
 sleep(DELAY)
 
-post(DISCORD+"/commands", headers=HEADERS, json={
+post(DISCORD + "/commands", headers=HEADERS, json={
     "name": "invite",
     "description": "Invite me to your own server"
 })
 sleep(DELAY)
 
-post(DISCORD+"/commands", headers=HEADERS, json={
+post(DISCORD + "/commands", headers=HEADERS, json={
     "name": "github",
     "description": "Get a link to my Github repository"
 })
 sleep(DELAY)
 
-post(DISCORD+"/commands", headers=HEADERS, json={
+post(DISCORD + "/commands", headers=HEADERS, json={
     "name": "town",
     "description": "Gets info about a town",
     "options": [{
@@ -39,7 +39,7 @@ post(DISCORD+"/commands", headers=HEADERS, json={
 })
 sleep(DELAY)
 
-post(DISCORD+"/commands", headers=HEADERS, json={
+post(DISCORD + "/commands", headers=HEADERS, json={
     "name": "nation",
     "description": "Gets info about a nation",
     "options": [{
@@ -51,7 +51,7 @@ post(DISCORD+"/commands", headers=HEADERS, json={
 })
 sleep(DELAY)
 
-post(DISCORD+"/commands", headers=HEADERS, json={
+post(DISCORD + "/commands", headers=HEADERS, json={
     "name": "resident",
     "description": "Gets info about a player",
     "options": [{
@@ -63,8 +63,156 @@ post(DISCORD+"/commands", headers=HEADERS, json={
 })
 sleep(DELAY)
 
-post(DISCORD+"/commands", headers=HEADERS, json={
+post(DISCORD + "/commands", headers=HEADERS, json={
     "name": "settings",
     "description": "Shows your settings"
+})
+sleep(DELAY)
+
+post(DISCORD + "/commands", headers=HEADERS, json={
+    "name": "set",
+    "description": "Changes settings",
+    "options": [
+        {
+            "type": 2,
+            "name": "user",
+            "description": "Changes user settings",
+            "options": [
+                {
+                    "type": 1,
+                    "name": "dadmode",
+                    "description": "Enables or disables dad mode",
+                    "options": [
+                        {
+                            "type": 5,
+                            "name": "value",
+                            "description": "Value to set dad mode to",
+                            "required": True
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "type": 2,
+            "name": "server",
+            "description": "Changes server settings",
+            "options": [
+                {
+                    "type": 1,
+                    "name": "chainbreak",
+                    "description": "Set the role to be assigned to whoever breaks a chain",
+                    "options": [
+                        {
+                            "type": 8,
+                            "name": "role",
+                            "description": "The role to be assigned to whoever breaks a chain",
+                            "required": True
+                        }
+                    ]
+                },
+                {
+                    "type": 1,
+                    "name": "admin",
+                    "description": "Add or remove an admin role",
+                    "options": [
+                        {
+                            "type": 4,
+                            "name": "add/remove",
+                            "description": "Add or remove from admin roles",
+                            "required": True,
+                            "choices": [
+                                {
+                                    "name": "add",
+                                    "value": 1
+                                },
+                                {
+                                    "name": "remove",
+                                    "value": 0
+                                }
+                            ]
+                        },
+                        {
+                            "type": 8,
+                            "name": "role",
+                            "description": "Role to add or remove from admin roles",
+                            "required": True
+                        }
+                    ]
+                },
+                {
+                    "type": 1,
+                    "name": "moderator",
+                    "description": "Add or remove a moderator role",
+                    "options": [
+                        {
+                            "type": 4,
+                            "name": "add/remove",
+                            "description": "Add or remove from moderator roles",
+                            "required": True,
+                            "choices": [
+                                {
+                                    "name": "add",
+                                    "value": 1
+                                },
+                                {
+                                    "name": "remove",
+                                    "value": 0
+                                }
+                            ]
+                        },
+                        {
+                            "type": 8,
+                            "name": "role",
+                            "description": "Role to add or remove from moderator roles",
+                            "required": True
+                        }
+                    ]
+                },
+                {
+                    "type": 1,
+                    "name": "ticketsupport",
+                    "description": "Add or remove a ticket support role",
+                    "options": [
+                        {
+                            "type": 4,
+                            "name": "add/remove",
+                            "description": "Add or remove from ticket support roles",
+                            "required": True,
+                            "choices": [
+                                {
+                                    "name": "add",
+                                    "value": 1
+                                },
+                                {
+                                    "name": "remove",
+                                    "value": 0
+                                }
+                            ]
+                        },
+                        {
+                            "type": 8,
+                            "name": "role",
+                            "description": "Role to add or remove from ticket support roles",
+                            "required": True
+                        }
+                    ]
+                },
+                {
+                    "type": 1,
+                    "name": "privatecommands",
+                    "description": "Set if slash command response should only show to the invoker",
+                    "options": [
+                        {
+                            "type": 5,
+                            "name": "enable/disable",
+                            "description": "Enable or disable private commands",
+                            "required": True
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 })
 sleep(DELAY)
