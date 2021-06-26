1b04989a4e56449e3260b6dbb6f1059666597335
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-06-26 10:07:50 +1000

    Added DMing Also fixed a few other things

--- bot/moderation.py
+++ bot/moderation.py
@@ -5,6 +5,7 @@ import time
 from discord.ext import commands, tasks
 from _util import Checks, set_db
 
+
 def parse_punishment(argument):  
     punishments = {
         "none": 0,
@@ -17,7 +18,7 @@ def parse_punishment(argument):
 
 
 def parse_time(argument):
-    time = re.match(r"(\d*)(s|m|h|d|w|M|y)", argument)
+    time_ = re.match(r"(\d*)([smhdwMy])", argument)
     times = {
         "s": 1,
         "m": 60,
@@ -27,9 +28,9 @@ def parse_time(argument):
         "M": 60**2 * 24 * 30,
         "y": 60**2 * 24 * 365
     }
-    if time is None:
+    if time_ is None:
         raise Exception
-    return int(time.group(1)) * times[time.group(2)]
+    return int(time_.group(1)) * times[time_.group(2)]
 
 
 def human_delta(duration):
@@ -64,10 +65,12 @@ class moderation(commands.Cog):
     
     @commands.command(aliases=["pu"])
     @commands.check(Checks.mod)
-    async def punish(self, ctx, users: commands.Greedy[discord.Member], punishment: typing.Optional[parse_punishment] = 0, duration: commands.Greedy[parse_time] = [], *, reason):
-        if punishment == 3 and duration != 0:
+    async def punish(self, ctx, users: commands.Greedy[discord.Member], punishment: typing.Optional[parse_punishment] = 0, duration: commands.Greedy[parse_time] = None, *, reason):
+        if duration is None:
+            duration = []
+        if punishment == 3 and duration != []:
             raise commands.BadArgument
-        
+
         duration = sum(duration)
         
         await ctx.message.delete()
@@ -82,7 +85,7 @@ class moderation(commands.Cog):
                                   "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", ctx.guild.id, id, ctx.author.id, [user.id for user in users],
                                   punishment, time.time(), time.time() + duration, reason, ref)
         
-        async def log(type):
+        async def log(type_):
             colours = {
                 "Warn": 0xfc9003,
                 "Mute": 0xc74c00,
@@ -90,10 +93,10 @@ class moderation(commands.Cog):
                 "Ban": 0xff0000
             }
 
-            embed = discord.Embed(title=f"Incident #{id}", colour=colours[type])
+            embed = discord.Embed(title=f"Incident #{id}", colour=colours[type_])
             embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
             embed.add_field(name="Members involved", value=", ".join([member.mention for member in users]))
-            embed.add_field(name="Punishemnt", value=type)
+            embed.add_field(name="Punishemnt", value=type_)
             if punishment in (1, 2, 4):
                 embed.add_field(name="Duration", value=human_delta(duration))
             embed.add_field(name="Reason", value=f"{reason} ([ref]({ref}))", inline=False)
@@ -101,24 +104,33 @@ class moderation(commands.Cog):
             for channel in [ctx.guild.get_channel(record["id"]) for record in await self.bot.db.fetch("SELECT id FROM channels WHERE log_type = 'moderation' AND guild_id = $1", ctx.guild.id)]:
                 await channel.send(embed=embed)
 
+        async def dm(title, message, colour):
+            embed = discord.Embed(title=title, description=message, colour=colour)
+            for user in users:
+                await user.send(embed=embed)
+
         async def none():
             pass
 
         async def warn():
+            await dm("Warning!", f"You have been warned in {ctx.guild.name} for {reason} for {human_delta(duration)}! Incident #{id} ([ref]({ref}))", 0xfc9003)
             await log("Warn")
 
         async def mute():
+            await dm("Muted!", f"You have been muted in {ctx.guild.name} for {reason} for {human_delta(duration)}! Incident #{id} ([ref]({ref}))", 0xc74c00)
             role = ctx.guild.get_role(await self.bot.db.fetchval("SELECT mute_role FROM guilds WHERE id = $1", ctx.guild.id))
             for user in users:
                 await user.add_roles(role, reason=f"Duration: {human_delta(duration)}. Incident #{id}: {reason}")
             await log("Mute")
 
         async def kick():
+            await dm("Kicked!", f"You have been kicked from {ctx.guild.name} for {reason}! Incident #{id} ([ref]({ref}))", 0xff0077)
             for user in users:
                 await user.kick(reason=f"Incident #{id}: {reason}")
             await log("Kick")
 
         async def ban():
+            await dm("Banned!", f"You have been banned from {ctx.guild.name} for {reason} for {human_delta(duration)}! Incident #{id} ([ref]({ref}))", 0xff0000)
             for user in users:
                 await user.ban(reason=f"Duration: {human_delta(duration)}. Incident #{id}: {reason}", delete_message_days=0)
             await log("Ban")
