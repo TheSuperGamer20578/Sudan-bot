a0d43fd3b6a441a5a928d1427ffaa0292f304ab9
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-07-31 19:55:56 +1000

    Made on message stuff in fun use pools

--- bot/Fun.py
+++ bot/Fun.py
@@ -2,7 +2,6 @@
 Contains fun stuff
 """
 import re
-import asyncio
 
 import discord
 from discord.ext import commands
@@ -59,48 +58,48 @@ class Fun(commands.Cog):
         """
         Checks to see if chain is broken
         """
-        chain = await self.bot.db.fetchval("SELECT chain FROM channels WHERE id = $1", msg.channel.id)
+        async with self.bot.pool.acquire() as db:
+            chain = await db.fetchval("SELECT chain FROM channels WHERE id = $1", msg.channel.id)
+            length = await db.fetchval("SELECT chain_length FROM channels WHERE id = $1", msg.channel.id)
 
-        async def cbreak():
-            """
-            If the chain is broken say so and update DB
-            """
-            if msg.content.endswith(f"chain {chain}"):
+            async def cbreak():
+                """
+                If the chain is broken say so and update DB
+                """
+                if msg.content.endswith(f"chain {chain}"):
+                    return
+                await msg.add_reaction("❌")
+                await msg.channel.send(f"{msg.author.mention} broke the chain! start a new chain with `.chain <thing>`. Chain length: {length}")
+                break_role = await db.fetchval("SELECT chain_break_role FROM guilds WHERE id = $1", msg.guild.id)
+                if break_role is not None:
+                    await msg.author.add_roles(msg.guild.get_role(break_role))
+                await db.execute("UPDATE channels SET chain = NULL, chain_length = 0 WHERE id = $1", msg.channel.id)
+
+            if chain is None or msg.author.bot:
                 return
-            await msg.add_reaction("❌")
-            await msg.channel.send(
-                f"{msg.author.mention} broke the chain! start a new chain with `.chain <thing>`. Chain length: {await self.bot.db.fetchval('SELECT chain_length FROM channels WHERE id = $1', msg.channel.id)}")
-            break_role = await self.bot.db.fetchval("SELECT chain_break_role FROM guilds WHERE id = $1", msg.guild.id)
-            if break_role is not None:
-                await msg.author.add_roles(msg.guild.get_role(break_role))
-            await self.bot.db.execute(
-                "UPDATE channels SET chain = NULL, chain_length = 0 WHERE id = $1",
-                msg.channel.id)
-
-        if chain is None or msg.author.bot:
-            return
-        if chain.startswith("$counting:"):
-            num = int(chain.split(":")[1])
-            if msg.content != str(num + 1):
+            if chain.startswith("$counting:"):
+                num = int(chain.split(":")[1])
+                if msg.content != str(num + 1):
+                    await cbreak()
+                else:
+                    num += 1
+                    await db.execute("UPDATE channels SET chain = $2 WHERE id = $1", msg.channel.id, f"$counting:{num}")
+            elif msg.content != chain or msg.author.id == await db.fetchval("SELECT last_chain FROM channels WHERE id = $1", msg.channel.id):
                 await cbreak()
             else:
-                num += 1
-                await self.bot.db.execute("UPDATE channels SET chain = $2 WHERE id = $1", msg.channel.id, f"$counting:{num}")
-        elif msg.content != chain or msg.author.id == await self.bot.db.fetchval("SELECT last_chain FROM channels WHERE id = $1", msg.channel.id):
-            await cbreak()
-        else:
-            await self.bot.db.execute("UPDATE channels SET last_chain = $2, chain_length = chain_length + 1 WHERE id = $1", msg.channel.id, msg.author.id)
-            if msg.content == "100":
-                await msg.add_reaction("💯")
-            else:
-                await msg.add_reaction("✅")
+                await db.execute("UPDATE channels SET last_chain = $2, chain_length = chain_length + 1 WHERE id = $1", msg.channel.id, msg.author.id)
+                if msg.content == "100":
+                    await msg.add_reaction("💯")
+                else:
+                    await msg.add_reaction("✅")
 
     async def dad_mode(self, message):
         """
         dad mode
         """
-        if not await self.bot.db.fetchval("SELECT dad_mode FROM users WHERE id = $1", message.author.id):
-            return
+        async with self.bot.pool.acquire() as db:
+            if not await db.fetchval("SELECT dad_mode FROM users WHERE id = $1", message.author.id):
+                return
         phrases = {
             "i am {}": "Hi {}! I am Dad",
             "i'm {}": "Hi {}! I'm Dad",
@@ -118,9 +117,7 @@ class Fun(commands.Cog):
         """
         runs stuff that has to run on_message
         """
-        await asyncio.sleep(.1)
         await self.dad_mode(message)
-        await asyncio.sleep(.1)
         await self.chain_check(message)
 
 
